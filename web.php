5913352dc2a26261328cadf86acc10f09ff2b965
Malik Al Ibrahim <133199700+Malik221948@users.noreply.github.com>
2023-10-06 16:00:51 +0200

    Http

--- web.php
+++ web.php
@@ -17,9 +17,36 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    $hello = ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
-    return  $hello;
-  
-});
+use Illuminate\Http\Request;
+Route::get('/planets', function (request $request) {
 
+    
+    $planets = [
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
+        ],
+        [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+        ],
+    ];
+    
+    if (request()->has('name')) {
+        
+        $colect = collect($planets)->where('name', ucfirst(request('name')));
+        dd($colect);
+        //$filtered = $colect->where('name', 'Mars');
+        return view($filtered);
+
+    }
+
+});
